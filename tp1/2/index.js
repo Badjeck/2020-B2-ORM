9661ec853a5a6b2c66ba3036f76126c0f917cfb3
Gabriel F <43410045+Badjeck@users.noreply.github.com>
2019-10-23 10:55:51 +0200

    Add files via upload

--- tp1/2/index.js
+++ tp1/2/index.js
@@ -7,7 +7,22 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
+    let nbcarton = parseInt(input[0]);
 
+    for (i=0 ; i => nbcarton ; i++){
+      let poid = parseInt(input[i+1]);
+      let charge
+      let alle = 0
+      
+      if (charge > 100){
+        charge - parseInt(input[i+1]);
+        alle++
+        charge += parseInt(input[i]);
+      }else{
+        charge += poid;
+      }
+    }
+    console.log(alle);
     // AND HERE
   },
   verify: function (dataset, output) {
