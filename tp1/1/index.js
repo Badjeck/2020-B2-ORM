218ff9a3524447287d7876b162bb3bed149c2cda
Gabriel F <43410045+Badjeck@users.noreply.github.com>
2019-10-23 10:55:26 +0200

    Add files via upload

--- tp1/1/index.js
+++ tp1/1/index.js
@@ -6,7 +6,28 @@ exports = module.exports = {
   name: "Marathon",
   datasets,
   algo: function (input) {
-    // YOUR CODE BETWEEN HERE
+    // YOUR CODE BETWEEN HERE 
+    let argent
+    let place = input[0]
+    
+    for (let i=1; i >= 43; i++){
+      let depasse = input[i]
+      
+      place += parseInt(depasse.substr(0));
+      place -= parseInt(depasse.substr(2));
+    }
+
+
+    if (place > 10000){
+      argent = "KO";
+    }else if (place <= 10000 && place > 100){
+      argent = 100;
+    } else {
+      argent = 1000;
+    }
+    console.log(argent);
+
+    //j'arrive pas a lancer le code donc je te fait ça a l'aveugle dsl
 
     // AND HERE
   },
